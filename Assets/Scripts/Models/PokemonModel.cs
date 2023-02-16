0e6fa639df95e353fb8dba8dc705ba7916fb8e7d
xanian7 <xanian407@gmail.com>
2023-02-16 16:03:47 -0500

    trying to sync changes

---
+++ Assets/Scripts/Models/PokemonModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Models
+{
+    public class PokemonModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public string Description { get; set; }
+        public Sprite frontSprite { get; set; }
+        public Sprite backSprite { get; set; }
+        public string Type1 { get; set; }
+        public string Type2 { get; set; }
+        public int MaxHp { get; set; }
+        public int Attack { get; set; }
+        public int SpAttack { get; set; }
+        public int Defense { get; set; }
+        public int SpDefense { get; set; }
+        public int Speed { get; set; }
+    }
+}
---
+++ Assets/Scripts/Pokemon/PokemonTypes.cs
@@ -0,0 +1,25 @@
+﻿namespace Assets.Scripts.Pokemon
+{
+    public enum PokemonTypes
+    {
+        None,
+        Normal,
+        Fire,
+        Water,
+        Grass,
+        Electric,
+        Ice,
+        Fighting,
+        Poison,
+        Ground,
+        Flying,
+        Psychic,
+        Bug,
+        Rock,
+        Ghost,
+        Dark,
+        Dragon,
+        Steel,
+        Fairy
+    }
+}
