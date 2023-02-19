0bfdede4327d5731c6383b57454169f210269903
xanian7 <xanian407@gmail.com>
2023-02-18 19:11:13 -0500

    fixing object error

--- Assets/Scripts/Player/PlayerPokemon.cs
+++ Assets/Scripts/Player/PlayerPokemon.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Assets.Scripts.Pokemon;
+using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Assets.Scripts.Player
 {
     public class PlayerPokemon
     {
-        List
+        List<Object> list;
 
 
     }
